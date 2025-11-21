32fa1cc3f7a5b3162169e83c5cccb6479b9c86f7
Jerdon07 <jdlitaoen@gmail.com>
2025-11-21 11:33:21 +0800

    Laravel Breeze installed for user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,27 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\FarmersController;
-use App\Http\Controllers\MeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
-Route::get('/', DashboardController::class);
-
-Route::get('/farmers', FarmersController::class);
-
-Route::get('/me', MeController::class);
-
-
-
-
-
-Route::get('/welcome', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
